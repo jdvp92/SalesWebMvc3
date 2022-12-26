3c469ed71ed2c26d6a35119d02d0647410f886e9
Jonathan Silva <j.dvp92@outlook.com>
2022-12-26 13:13:40 -0300

    CRUD Scaffolding

--- SalesWebMvc3/Controllers/HomeController.cs
+++ SalesWebMvc3/Controllers/HomeController.cs
@@ -43,3 +43,4 @@ namespace SalesWebMvc3.Controllers
         }
     }
 }
+   
--- SalesWebMvc3/Startup.cs
+++ SalesWebMvc3/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc3.Data;
 
 namespace SalesWebMvc3
 {
@@ -33,6 +35,9 @@ namespace SalesWebMvc3
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMvc3Context>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvc3Context")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
